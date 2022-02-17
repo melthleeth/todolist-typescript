7b844cc4c03fa1f9fa2acf629cb942685ab7442d
Hyejin Lee <hichjay@gmail.com>
2022-02-18 00:53:02 +0900

    feat: main.ts 코드 작성중

--- todolist/src/main.ts
+++ todolist/src/main.ts
@@ -1,8 +1,34 @@
-import './style.css'
+// import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+class TaskInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "task-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attatch();
+  }
+
+  private attatch() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const taskInput = new TaskInput();
