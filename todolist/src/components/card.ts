b2511f4b3e87d2b2903d1474609212d7b6dac3bc
Hyejin Lee <hichjay@gmail.com>
2022-02-22 02:04:06 +0900

    feat: card - 메인 객체

---
+++ todolist/src/components/card.ts
@@ -0,0 +1,45 @@
+import Component from "./base-component";
+import TaskInput from "./task-input";
+import TaskListInfo from "./task-list-info";
+import { taskState } from "../state/task-state";
+import { Task } from "../models/task";
+import { TaskItem } from "./task-item";
+
+export default class Card extends Component<HTMLDivElement, HTMLDivElement> {
+  tasks: Task[];
+
+  get taskCount() {
+    return this.element
+      .querySelector("#task-list")!
+      .getElementsByTagName("li")
+      .length.toString();
+  }
+
+  constructor() {
+    super("task-card", "app", true, "card");
+    this.tasks = [];
+
+    new TaskListInfo();
+    new TaskInput();
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {
+    taskState.addListener((tasks: Task[]) => {
+      this.tasks = tasks;
+      this.renderContent();
+    });
+  }
+  renderContent() {
+    const listEl = document.getElementById("task-list")! as HTMLUListElement;
+    listEl.innerHTML = "";
+    console.log("tasks", this.tasks);
+    for (const taskItem of this.tasks) {
+      new TaskItem(this.element.querySelector("ul")!.id, taskItem);
+    }
+
+    this.element.querySelector("#lefttask")!.textContent = this.taskCount;
+  }
+}
